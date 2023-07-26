bc7f25e86bedf8c0ddc9c9b0ba1ab50992e47fcd
Junior <ismaeljunior1508@gmail.com>
2023-07-26 14:28:56 -0300

    quantidadeCafeArrumado

--- src/reducers/reducer.ts
+++ src/reducers/reducer.ts
@@ -4,23 +4,36 @@ interface CoffesReducer {
   coffes: CoffeType[];
   numberCoffe: number;
 }
-export function CoffesReducer(state: CoffesReducer, action: any) {
+interface ActionCoffesReduce {
+  type: string; // Replace "string" with the actual type of ActionTypes
+  playload: {
+    coffe: CoffeType;
+  };
+}
+export function CoffesReducer(
+  state: CoffesReducer,
+  action: ActionCoffesReduce,
+) {
   switch (action.type) {
-    case ActionTypes.ADD_NEW_COFFE:
+    case ActionTypes.ADD_NEW_COFFE: {
       return {
         ...state,
         coffes: [...state.coffes, action.playload.coffe],
         numberCoffe: state.numberCoffe + 1,
       };
-    case ActionTypes.REMOVE_COFFE:
+    }
+    case ActionTypes.REMOVE_COFFE: {
+      const coffeRemoveIndex = state.coffes.findIndex(
+        (coffe) => coffe.id === action.playload.coffe.id,
+      );
+
       return {
         ...state,
-        coffes: state.coffes.filter(
-          (coffe) => coffe.data !== action.playload.data,
-        ),
+        coffes: state.coffes.filter((_, index) => index !== coffeRemoveIndex),
         numberCoffe:
-          state.numberCoffe > 0 ? state.numberCoffe - 1 : state.numberCoffe,
+          coffeRemoveIndex !== -1 ? state.numberCoffe - 1 : state.numberCoffe,
       };
+    }
     default:
       return state;
   }
