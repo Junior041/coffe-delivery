4e219090d96f4715782e875919068c252d43a707
Junior <ismaeljunior1508@gmail.com>
2023-07-27 13:47:26 -0300

    contas

--- src/pages/Checkout/CafesSelecionados/TotalItems/index.tsx
+++ src/pages/Checkout/CafesSelecionados/TotalItems/index.tsx
@@ -1,19 +1,30 @@
+import { useContext } from 'react';
 import { TotalItemsContainer } from './styles';
+import { CoffeContext } from '../../../../contexts/CoffeContext';
 
 export default function TotalItems() {
+  const { coffes } = useContext(CoffeContext);
+
+  let precoItems = 0;
+  const precoEntrega = '4';
+  coffes.map((coffe) => {
+    precoItems = precoItems + Number(coffe.preco);
+    return true;
+  });
+
   return (
     <TotalItemsContainer>
       <div>
         <span>Total de itens</span>
-        <span>R$ 29,70</span>
+        <span>R$ {precoItems.toFixed(2)}</span>
       </div>
       <div>
         <span>Entrega</span>
-        <span>R$ 3,50</span>
+        <span>R$ {Number(precoEntrega).toFixed(2)}</span>
       </div>
       <div>
         <span>Total</span>
-        <span>R$ 33,20</span>
+        <span>R$ {(Number(precoEntrega) + precoItems).toFixed(2)}</span>
       </div>
     </TotalItemsContainer>
   );
--- src/pages/Checkout/CafesSelecionados/TotalItems/styles.ts
+++ src/pages/Checkout/CafesSelecionados/TotalItems/styles.ts
@@ -5,4 +5,19 @@ export const TotalItemsContainer = styled.div`
   flex-direction: column;
   align-items: initial;
   padding: 8px;
+  gap: 12px;
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: ${(props) => props.theme['gray-800']};
+  }
+  div:last-child {
+    color: ${(props) => props.theme['gray-800']};
+    font-family: 'Roboto';
+    font-size: 1.25rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 130%; /* 26px */
+  }
 `;
